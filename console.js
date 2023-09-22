c017567640edc6c7402e0b86bae011ae25fe854e
FlyInTheBox <d19wylazowskiw@gmail.com>
2023-09-22 13:44:16 +0200

    a

---
+++ console.js
@@ -0,0 +1,8 @@
+console.log("błąd",404);
+console.log(window);
+console.table(["zero","jeden","dwa"]);
+console.group("bulbulator");
+console.log("falowana blaszka");
+console.log("z dziurkami");
+console.log("która polana wodą wydaje odgłos bulbulbul");
+console.groupEnd()
