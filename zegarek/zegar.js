03e2d63413c447931b89e5b1b3400679a8cbe9ff
FlyInTheBox <d19wylazowskiw@gmail.com>
2023-11-20 10:52:27 +0100

    a

--- zegarek/zegar.js
+++ zegarek/zegar.js
@@ -1,6 +1,6 @@
 function changeHour() {
-    var valueHours = document.getElementById('hours_range').value;
-    var angleHours = 30 * valueHours + 'deg';
+    let valueHours = document.getElementById('hours_range').value;
+    let angleHours = 30 * valueHours + 'deg';
     document.getElementById('hours_output').innerText = valueHours;
     document.getElementById('hours_hand').style.transform = 'rotate(' + angleHours + ')';
     //document.getElementById('hours_range').style.transform = 'rotate(' + angleHours + ')';
@@ -8,16 +8,16 @@ function changeHour() {
 }
 
 function changeMin() {
-    var valueMinutes = document.getElementById('minutes_range').value;
-    var angleMinutes = 6 * valueMinutes + 'deg';
+    let valueMinutes = document.getElementById('minutes_range').value;
+    let angleMinutes = 6 * valueMinutes + 'deg';
     document.getElementById('minutes_output').innerText = valueMinutes;
     document.getElementById('minutes_hand').style.transform = 'rotate(' + angleMinutes + ')';
     //document.getElementById('minutes_range').style.transform = 'rotate(' + angleMinutes + ')';
     update();
 }
 function update() {
-    var valueHours = document.getElementById('hours_range').value;
-    var valueMinutes = document.getElementById('minutes_range').value;
-    angle = Math.abs(30 * valueHours - 11/2 * valueMinutes);
+    let valueHours = document.getElementById('hours_range').value;
+    let valueMinutes = document.getElementById('minutes_range').value;
+    let angle = Math.abs(30 * valueHours - 11/2 * valueMinutes);
     document.getElementById('angle').innerText = angle + '°';
 }
